f059a637cd0bef144a85b8b73f0fbecab0442575
Timmyy3000 <timiogunme@gmail.com>
2021-06-19 22:56:12 +0100

    Created room model

--- groovehub/api/models.py
+++ groovehub/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    
+    code = models.CharField(max_length=10, default="", unique= True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null = False, default = False)
+    votes_to_skip = models.IntegerField(null = False, default = 1)
+    date_time_created = models.DateTimeField(auto_now_add=True)
