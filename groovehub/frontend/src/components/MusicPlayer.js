fb244637dd7bfa95e48fca6e0bc04791993f4445
Timmyy3000 <timiogunme@gmail.com>
2021-06-22 21:01:08 +0100

    Music Player

--- groovehub/frontend/src/components/MusicPlayer.js
+++ groovehub/frontend/src/components/MusicPlayer.js
@@ -1,16 +1,25 @@
-import { Card, Grid, IconButton, Typography } from '@material-ui/core'
-import {PlayArrowIcon,  SkipNextIcon, PauseIcon } from '@material-ui/icons';
-import React from 'react'
+import { Card, Grid, IconButton, Typography } from "@material-ui/core";
+import { PlayArrowIcon, SkipNextIcon, PauseIcon } from "@material-ui/icons";
+import React from "react";
 
-const MusicPlayer = () => {
+const MusicPlayer = (props) => {
+  return (
+    <Card>
+      <Grid container alignItems="center">
+        <Grid item align="center" xs={4}>
+          <img src={props.song.image_url} height="100%" width="100%" />
+        </Grid>
+        <Grid item align="center" xs={8}>
+          <Typography component="h5" variant="h5">
+            {props.song.title}
+          </Typography>
+          <Typography color="textSecondary" component="h5" variant="h5">
+            {props.song.artists}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Card>
+  );
+};
 
-    return (
-        <Card>
-            <Grid container alignItems="center">
-
-            </Grid>
-        </Card>
-    )
-}
-
-export default MusicPlayer
+export default MusicPlayer;
