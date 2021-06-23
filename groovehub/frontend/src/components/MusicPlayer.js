73459682ad03678385bc235d7a7bacdd20147534
Timmyy3000 <timiogunme@gmail.com>
2021-06-23 09:00:38 +0100

    Music PLayer styling

--- groovehub/frontend/src/components/MusicPlayer.js
+++ groovehub/frontend/src/components/MusicPlayer.js
@@ -12,10 +12,10 @@ const MusicPlayer = (props) => {
   return (
     <Card>
       <Grid container alignItems="center">
-        <Grid item align="center" xs={6}>
-          <img src={props.song.image_url} height="50%" width="50%" />
-        </Grid>
-        <Grid item align="center" xs={6}>
+      <Grid item align="center" xs={4}>
+            <img src={props.song.image_url} height="100%" width="100%" />
+          </Grid>
+        <Grid item align="center" xs={8}>
           <Typography component="h5" variant="h5">
             {props.song.title}
           </Typography>
